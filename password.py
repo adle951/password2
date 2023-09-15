d638e3dccabad7e8c6dd46d831c2b743e25ad5e2
adle951 <110898970+adle951@users.noreply.github.com>
2023-09-16 01:30:42 +0800

    second commit

--- password.py
+++ password.py
@@ -16,3 +16,10 @@ while True:
 
 
 
+# echo "# password2" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/adle951/password2.git
+# git push -u origin main
